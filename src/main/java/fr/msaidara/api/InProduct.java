c982315cf08187a67a6ed92f8dc0a5fc2d521fc2
Aidara Moussa <msaidara@gmail.com>
2017-06-28 09:39:33 +0200

    Create InProduct.java

---
+++ src/main/java/fr/msaidara/api/InProduct.java
@@ -0,0 +1,51 @@
+package fr.msaidara.api;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlType(name = "InProduct")
+public class InProduct {
+	
+	@XmlElement(name = "id", required = false)
+	private int id;
+	
+	@XmlElement(name = "name", required = false)
+	private String name;
+	
+	@XmlElement(name = "price", required = false)
+	private double price;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Double getPrice() {
+		return price;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "id=" + id + ", name=" + name + ", price=" + price;
+	}
+	
+}
